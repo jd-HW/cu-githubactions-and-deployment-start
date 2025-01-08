c928fec3f9b475820ad3efd280f55ae1841a0ba9
jd-HW <johannes.dereuddre@student.howest.be>
2025-01-08 17:08:23 +0100

    Fix erreur

--- CICD.Mvc/Controllers/HomeController.cs
+++ CICD.Mvc/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ namespace CICD.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            he
+            
         }
 
 
